363782f97f6989c9202f9b189a8076756542ff09
websiteaap <arun887126@gmail.com>
2021-12-22 22:03:05 +0530

    git

--- YoutubeDownloader/settings.py
+++ YoutubeDownloader/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xoyzt_eg$)zxra8&@s__et@!5_d8kq+*ci$1yh^+y5r&vr&we2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["https://downuptube.herokuapp.com", "localhost", "127.0.0.1"]
 
