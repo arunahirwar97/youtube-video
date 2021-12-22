a9da19faf6a62c18ca649940f62e0da580c231c7
websiteaap <arun887126@gmail.com>
2021-12-22 21:55:05 +0530

    git

--- Youtube/views.py
+++ Youtube/views.py
@@ -10,56 +10,6 @@ def index(request):
     print("outside loop")
     if request.method == 'POST':
         link = request.POST.get("youtubeLink")
-        downloadAudio = request.POST.get("downloadAudio")
-        downloadVideo = request.POST.get("downloadVideo")
-        audioQuality = request.POST.get("AudioQuality")
-        videoQuality = request.POST.get("VideoQuality")
-        print("hello")
-        print(audioQuality, videoQuality)
-        print(downloadAudio, downloadVideo)
-        print("link "+str(link))
-        if link:
-            yt = YouTube(link)
-            title = yt.title
-            thumbnail = yt.thumbnail_url
-
-            audio = []
-            video = []
-            for i in yt.streams.filter(only_audio=True):
-                print(i)
-                audio.append(i.abr)
-            print("video ")
-
-            for j in yt.streams.filter(only_video=True, mime_type="video/mp4"):
-                print(j)
-                video.append(j.resolution)
-
-            audio.sort()
-            video = list(set(video))
-            video.sort()
-            print(audio, video)
-            print(thumbnail)
-            return render(request, 'index.html', {
-                'thumbnail': thumbnail,
-                'title': title,
-                'audio': audio,
-                'video': video,
-                'link': link,
-            })
-        elif downloadAudio:
-            if audioQuality != "Audio Quality" and downloadAudio is not None:
-                print("Download")
-                yt = YouTube(downloadAudio)
-                yt = yt.streams.filter(abr=audioQuality)
-                yt.first().download(dirs)
-                return render(request, 'index.html', {
-                    'message': "Downloaded ..... ",
-                })
-            else:
-                print("select audio quality")
-                return render(request, 'index.html', {
-                    'error': "Choose Audio Quality",
-                })
-        elif downloadVideo:
-	        return FileResponse(open(YouTube(link).streams.first().download(skip_existing=True),'rb'))	
+        print(link)
+	    return FileResponse(open(YouTube(link).streams.first().download(skip_existing=True),'rb'))	
     return render(request, 'index.html')
