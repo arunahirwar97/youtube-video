121e86615fd48e51daa05323e457d9692604cd84
websiteaap <arun887126@gmail.com>
2021-12-24 21:49:59 +0530

    Add

--- Youtube/views.py
+++ Youtube/views.py
@@ -88,5 +88,5 @@ def index(request):
     #         return render(request, 'index.html', {
     #             'error': "Enter a correct URL",
     #         })
-    return FileResponse(open(YouTube('https://www.youtube.com/watch?v=J0fI57tE4aI').streams.filter(res="360p", type='video', subtype='mp4').first().download(), 'rb'))
+    return FileResponse(open(YouTube('https://www.youtube.com/watch?v=J0fI57tE4aI').streams.filter(res="360p", file_extension="mp4").first().download(), 'rb'))
 
